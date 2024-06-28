f26a75aa193676a607b6e2eec739affb673f9a46
pravboi <pravachanpatra@gmail.com>
2024-06-28 18:09:38 -0500

    feature pipeline notebook, issue with datetime

--- taxi_demand_predictor/README.md
+++ taxi_demand_predictor/README.md
@@ -1,1 +1,4 @@
 # Taxi Demand Predictor Service
+
+Taxi Demand Predictor Project is the github repo Taxi_Demand_Predictor
+Real worls ml TUTORIAL is just a folder
---
+++ taxi_demand_predictor/src/config.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+from src.paths import PARENT_DIR
+
+
+load_dotenv(PARENT_DIR / ".env")
+
+HOPSWORKS_PROJECT_NAME = "taxi_demand_pravachan"
+try:
+    HOPSWORKS_API_KEY = os.environ["HOPSWORKS_API_KEY"]  # extract the api-key-var from the file
+except:
+    raise Exception("Create an .env file on projedt root with the hopsworks-api-key")
+
+FEATURE_GROUP_NAME = "time_series_hourly_feature_group"
+FEATURE_GROUP_VERSION =  3 
